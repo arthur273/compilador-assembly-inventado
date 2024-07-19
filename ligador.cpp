a75310bacac3711966eebdcf8e70504887ff737a
josebaraujo2 <josebarbosaaraujo8@gmail.com>
2024-07-19 01:10:02 -0300

    arquivodoligador

---
+++ ligador.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// para compilar
+// g++ -o ligador ligador.cpp
+// ./ligador prog1.obj
+
+int main(int argc, char* argv[]) {
+
+    if (argc < 2) {
+    cout << "ERRO: NAO FOI PASSADO ARGUMENTO PARA O LIGADOR." << endl;
+    return 0;
+    }
+
+    string arquivo_obj = argv[1];
+    // implementar ligador
+
+    cout << "TESTE";
+
+    return 0;
+}
